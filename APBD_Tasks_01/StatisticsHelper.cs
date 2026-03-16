55989dcf4a4ecc5d9476dc7b2ac635e854e11f2e
Daria Serhiienko <s30246@pjwstk.edu.pl>
2026-03-16 12:12:59 +0100

    Add error

--- APBD_Tasks_01/StatisticsHelper.cs
+++ APBD_Tasks_01/StatisticsHelper.cs
@@ -4,6 +4,10 @@ public class StatisticsHelper
 {
     public static double CalculateAverage(int[] values)
     {
+        if (values == null || values.Count == 0)
+        {
+            throw new ArgumentException("List cannot be empty");
+        }
         return values.Average();
     }
 }

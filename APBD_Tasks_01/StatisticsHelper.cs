860a59d809dda76eef77f0553d2eb39c55af6a86
Daria Serhiienko <s30246@pjwstk.edu.pl>
2026-03-16 12:29:39 +0100

    Add method for array

--- APBD_Tasks_01/StatisticsHelper.cs
+++ APBD_Tasks_01/StatisticsHelper.cs
@@ -4,9 +4,9 @@ public class StatisticsHelper
 {
     public static double CalculateAverage(int[] values)
     {
-        if (values == null || values.Count == 0)
+        if (values == null || values.length == 0)
         {
-            throw new ArgumentException("List cannot be empty");
+            throw new ArgumentException("Array cannot be empty");
         }
         return values.Average();
     }
